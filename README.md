c561efbc183a4c188daf69574aa2221084cb4195
Branyac <laguaridadelgeek@gmail.com>
2015-08-30 21:50:46 +0200

    Version 2.0
    Added webcam to do live streaming to the webpage, and the kernel modules
    for Sony Eye webcam on Yocto Linux.

--- README.md
+++ README.md
@@ -1,28 +1,32 @@
 # Intel Edison smartcar
-By Rafael Espejo and Sergio Monedero<br />
+By Sergio Monedero<br />
 Released on April 24th 2015<br />
 http://thingsandcode.com
 ***
 
-Build an Smart Car using an Intel Edison with an Arduino Breakout board and a L298N motor controller. Move the car using a web interface.
+Build an Smart Car using an Intel Edison with an Arduino Breakout board and a L298N motor controller. Move the car and see the camera live stream using a web interface.
 
 ## Build
-Connect Intel Edison board Arduino pins to the L298N controller in this way:
-* IN1 -- Pin 7
-* IN2 -- Pin 6
-* IN3 -- Pin 5
-* IN4 -- Pin 4
-* Vin -- Motor's power supply positive terminal (6 - 12v)
-* GND -- Motor's power supply positive terminal and GND pin of Arduino. 
-* OUT1 -- First motor
-* OUT4 -- Second motor
-
+1. Connect Intel Edison board Arduino pins to the L298N controller in this way:
+	* IN1 -- Pin 7
+	* IN2 -- Pin 6
+	* IN3 -- Pin 5
+	* IN4 -- Pin 4
+	* Vin -- Motor's power supply positive terminal (6 - 12v)
+	* GND -- Motor's power supply positive terminal and GND pin of Arduino. 
+	* OUT1 -- First motor
+	* OUT4 -- Second motor
+2. Set the small switch to the OFF position by pushing it toward the big USB port connector and connect the webcam.
+3. Power on the Intel Edison using the DC power jack.
 
 ## Installation
 1. Connect the Intel Edison board to a network with an static IP.
-2. Go to `Smartcar` folder and open `SmartCar.xdk` with Intel XDK IoT Edition.
-3. Click on `Install/Build` button of Intel XDK to fetch dependencies and compile the code.
-4. Click on `Upload` to send the program to the device.
+2. Go to `misc` folder and copy `install_ffmpeg.sh` to `/home/root` on your Intel Edison using a SCP client.
+3. [OPTIONAL] If you are using a Sony Eye webcam you need the kernel modules to make it work. Go to `misc/kernel_modules` folder and copy all the files to `/home/root` on your Intel Edison using a SCP client.
+4. Open a SSH, connect with your Intel Edison, set permissions to execute `install_ffmpeg.sh`, connect the Intel Edison to the Internet and execute the script.
+5. Go to `Smartcar` folder and open `SmartCar.xdk` with Intel XDK IoT Edition.
+6. Click on `Install/Build` button of Intel XDK to fetch dependencies and compile the code.
+7. Click on `Upload` to send the program to the device.
 
 
 ### Usage
--- SmartCar/main.js
+++ SmartCar/main.js
@@ -2,17 +2,28 @@
 
 /**
     SmartCar
-    By: Rafael Espejo and Sergio Monedero
+    By Sergio Monedero
     2015
 **/
 
 var APP_PORT = 8080;
+var WS_PORT = 8084;
+var VS_PORT = 8082;
+var STREAM_MAGIC_BYTES = 'jsmp'; // Must be 4 bytes
+var FFMPEG_PATH = '/home/root/bin/ffmpeg/ffmpeg';
+var PHOTO_FILE = '/tmp/photo.jpg';
+var THETHINGSIO_TOKEN = '';
 
 var http;
 var url;
 var mraa;
 var express;
 var app;
+var ws;
+var childProcess;
+var ffmpeg;
+
+var led_output;
 
 // Motor controller pins
 var m_input3;
@@ -20,24 +31,36 @@ var m_input4;
 var m_input5;
 var m_input6;
 
+// Webcam config
+var width = 640;
+var height = 480;
+var autoRestartStreaming;
+
 function setup_requires() {
     try {
-        console.log('Initializing libraries...');
+        log_message('Initializing libraries...');
 
         http = require("http");
-        console.log('HTTP initialized');
+        log_message('HTTP initialized');
 
         url = require('url');
-        console.log('URL initialized');
+        log_message('URL initialized');
 
         mraa = require('mraa'); //require mraa
-        console.log('MRAA Version: ' + mraa.getVersion()); //write the mraa version to the console
+        log_message('MRAA Version: ' + mraa.getVersion()); //write the mraa version to the console
 
         express = require('express');
-        console.log('Express framework initialized');
-        console.log('')
+        log_message('Express framework initialized');
+        
+        ws = require('ws');
+        log_message('WebSocket initialized');
+        
+        childProcess = require('child_process')
+        log_message('Child process initialized');
+
+        log_message('');
     } catch(err) {
-        console.log("ERROR: " + err.message);
+        log_message("ERROR: " + err.message);
     }
 }
 
@@ -52,10 +75,106 @@ function setup_app() {
           var host = server.address().address;
           var port = server.address().port;
 
-          console.log('intel-edison-smartcar listening at http://%s:%s', host, port);
+          log_message('intel-edison-smartcar listening at http://%s:%s', host, port);
         });
     } catch(err) {  
-        console.log("ERROR: " + err.message);
+        log_message("ERROR: " + err.message);
+    }
+}
+
+function setup_websockets() {
+    try {
+        // WebSocket server
+        var wsServer = new (ws.Server)({ port: WS_PORT });
+        log_message('WebSocket server listening on port ' + WS_PORT);
+
+        wsServer.on('connection', function(socket) {
+            try {
+              // Send magic bytes and video size to the newly connected socket
+              // struct { char magic[4]; unsigned short width, height;}
+              var streamHeader = new Buffer(8);
+
+              streamHeader.write(STREAM_MAGIC_BYTES);
+              streamHeader.writeUInt16BE(width, 4);
+              streamHeader.writeUInt16BE(height, 6);
+              socket.send(streamHeader, { binary: true });
+
+              log_message('New WebSocket Connection (' + wsServer.clients.length + ' total)');
+
+              socket.on('close', function(code, message){
+                log_message('Disconnected WebSocket (' + wsServer.clients.length + ' total)');
+              });
+            } catch(err) {  
+                log_message("ERROR: " + err.message);
+            }
+        });
+
+        wsServer.broadcast = function(data, opts) {
+            try {
+                for(var i in this.clients) {
+                    if(this.clients[i].readyState == 1) {
+                        this.clients[i].send(data, opts);
+                    } else {
+                        log_message('Error: Client (' + i + ') not connected.');
+                    }
+                }
+            } catch(err) {  
+                log_message("ERROR: " + err.message);
+            }
+        };
+        
+        // HTTP server to accept incoming MPEG1 stream
+        http.createServer(function (req, res) {
+            try {
+              log_message(
+                'Stream Connected: ' + req.socket.remoteAddress +
+                ':' + req.socket.remotePort + ' size: ' + width + 'x' + height
+              );
+
+              req.on('data', function (data) {
+                wsServer.broadcast(data, { binary: true });
+              });
+            } catch(err) {  
+                log_message("ERROR: " + err.message);
+            }
+        }).listen(VS_PORT, function () {
+            log_message('Listening for video stream on port ' + VS_PORT);
+            
+            autoRestartStreaming = true;
+            startStreaming();
+        });
+    } catch(err) {
+        log_message("ERROR: " + err.message);
+    }
+}
+
+function startStreaming() {
+    try {
+        // Load Sony Eye Driver modules
+        childProcess.exec('insmod /home/root/gspca_main.ko');
+        childProcess.exec('insmod /home/root/gspca_ov534.ko');
+        childProcess.exec('insmod /home/root/gspca_ov534_9.ko');
+
+        ffmpeg = childProcess.exec(FFMPEG_PATH + ' -s ' + width + 'x' + height + ' -f video4linux2 -i /dev/video0 -f mpeg1video -b 800k -r 30 http://127.0.0.1:' + VS_PORT);
+
+        ffmpeg.on('error', function (err) {
+            log_message("ERROR: " + err.message);
+        });
+
+        ffmpeg.on('close', function (code) {
+            log_message('ffmpeg exited with code ' + code);
+            //if(autoRestartStreaming === true) startStreaming();
+        });
+    } catch(err) {  
+        log_message("ERROR: " + err.message);
+    }
+}
+
+function stopStreaming() {
+    try {
+        ffmpeg.kill();
+    } catch(err) {
+        log_message("ERROR: " + err.message);
     }
 }
 
@@ -69,12 +188,11 @@ function setup_arduino_pins() {
         m_input5.dir(mraa.DIR_OUT);
         m_input6 = new mraa.Gpio(7);
         m_input6.dir(mraa.DIR_OUT);
-
-        var myDigitalPin5 = new mraa.Gpio(13); //setup digital read on Digital pin #5 (D5)
-        myDigitalPin5.dir(mraa.DIR_OUT); //set the gpio direction to output
-        myDigitalPin5.write(1); //set the digital pin to high (1)
+        
+        led_output = new mraa.Gpio(13);
+        led_output.dir(mraa.DIR_OUT);
     } catch(err) {
-        console.log("ERROR: " + err.message);
+        log_message("ERROR: " + err.message);
     }
 }
 
@@ -84,6 +202,7 @@ function doAction(request, response) {
         response.write("SmartCar");
 
         var motor_action = url.parse(request.url, true).query['motors_action'];
+        var take_photo_param = url.parse(request.url, true).query['take_photo'];
 
         if(typeof motor_action !== 'undefined') {
             response.write("motors_action=" + motor_action);
@@ -92,52 +211,99 @@ function doAction(request, response) {
         } else {
             response.write("no motors_action");
         }
+        
+        if(typeof take_photo_param !== 'undefined') {
+            autoRestartStreaming = false;
+            
+            stopStreaming();
+            take_photo();
+            
+            autoRestartStreaming = true;
+            startStreaming();
+            
+            response.write("photo saved");
+        }
+        
         response.end();
     } catch(err) {
-        console.log("ERROR: " + err.message);
+        log_message("ERROR: " + err.message);
     }
 }
 
 function main() {
-    setup_requires();
-    setup_arduino_pins();
-    setup_app();
+    try {
+        setup_requires();
+        setup_arduino_pins();
+        setup_websockets();
+        setup_app();
+    } catch(err) {
+        log_message("FATAL ERROR: " + err.message);
+    }
+}
+
+function take_photo() {
+    try {
+        if(typeof childProcess !== 'undefined' && typeof THETHINGSIO_TOKEN !== 'undefined' && THETHINGSIO_TOKEN != '') {
+            ffmpeg = childProcess.exec(FFMPEG_PATH + ' -f video4linux2 -i /dev/video0 -s 640x480 -vframes 1 ' + PHOTO_FILE);
+        }
+    } catch(err) {
+        log_message("ERROR: " + err.message);
+    }
 }
 
 function start_motors(action) {
-    if(action == 'L') {
-        console.log('Motors: Turn left.');
-        m_input4.write(1);
-        m_input3.write(0);
-        m_input6.write(0);
-        m_input5.write(1);
-    } else if (action == 'R') {
-        console.log('Motors: Turn right.');
-        m_input4.write(0);
-        m_input3.write(1);
-        m_input6.write(1);
-        m_input5.write(0);
-    } else if (action == 'B') {
-        console.log('Motors: Go backward.');
-        m_input4.write(1);
-        m_input3.write(0);
-        m_input6.write(1);
-        m_input5.write(0);
-    } else if (action == 'F') {
-        console.log('Motors: Go forward.');
-        m_input4.write(0);
-        m_input3.write(1);
-        m_input6.write(0);
-        m_input5.write(1);
+    try {
+        led_output.write(1);
+        
+        if(action == 'L') {
+            log_message('Motors: Turn left.');
+            m_input4.write(1);
+            m_input3.write(0);
+            m_input6.write(0);
+            m_input5.write(1);
+        } else if (action == 'R') {
+            log_message('Motors: Turn right.');
+            m_input4.write(0);
+            m_input3.write(1);
+            m_input6.write(1);
+            m_input5.write(0);
+        } else if (action == 'B') {
+            log_message('Motors: Go backward.');
+            m_input4.write(1);
+            m_input3.write(0);
+            m_input6.write(1);
+            m_input5.write(0);
+        } else if (action == 'F') {
+            log_message('Motors: Go forward.');
+            m_input4.write(0);
+            m_input3.write(1);
+            m_input6.write(0);
+            m_input5.write(1);
+        }
+
+        setTimeout(function() {
+            log_message('Motors: Stop.');
+            m_input4.write(0);
+            m_input3.write(0);
+            m_input6.write(0);
+            m_input5.write(0);
+            
+            led_output.write(0);
+        }, 1000);
+    } catch(err) {
+        log_message("ERROR: " + err.message);
     }
-    
-    setTimeout(function() {
-        console.log('Motors: Stop.');
-        m_input4.write(0);
-        m_input3.write(0);
-        m_input6.write(0);
-        m_input5.write(0);
-    }, 1000);
+}
+
+function log_message(message) {
+    try {
+        console.log(message);
+        if(typeof childProcess !== 'undefined' && typeof THETHINGSIO_TOKEN !== 'undefined' && THETHINGSIO_TOKEN != '') {
+            childProcess.exec('curl -i -H "Accept: application/json"  -H "Content-Type: application/json" -d \'{ "values": [ { "key": "log", "value": "' + message + '" } ] }\' -X POST "https://api.thethings.io/v2/things/' + THETHINGSIO_TOKEN + '" -k');
+        }
+    } catch(err) {
+        console.log("ERROR: " + err.message);
+    }
 }
 
 main();
